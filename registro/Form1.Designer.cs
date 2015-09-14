dbb95693c5fdf3433500e6a9943c7b173b2daacd
edwinh150 <edwinh2410@gmail.com>
2015-09-14 17:05:06 -0400

    Prueba Para Enel.

---
+++ registro/Form1.Designer.cs
@@ -0,0 +1,469 @@
+﻿namespace registro
+{
+    partial class formRegistro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRegistrar = new System.Windows.Forms.Button();
+            this.TituloTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textProduccion = new System.Windows.Forms.TextBox();
+            this.comboPais = new System.Windows.Forms.ComboBox();
+            this.comboGenero = new System.Windows.Forms.ComboBox();
+            this.richTextDescripcion = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textDirector = new System.Windows.Forms.TextBox();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // buttonRegistrar
+            // 
+            this.buttonRegistrar.Location = new System.Drawing.Point(130, 419);
+            this.buttonRegistrar.Name = "buttonRegistrar";
+            this.buttonRegistrar.Size = new System.Drawing.Size(112, 29);
+            this.buttonRegistrar.TabIndex = 0;
+            this.buttonRegistrar.Text = "Registrar";
+            this.buttonRegistrar.UseVisualStyleBackColor = true;
+            this.buttonRegistrar.Click += new System.EventHandler(this.buttonR_Click);
+            // 
+            // TituloTextBox
+            // 
+            this.TituloTextBox.Location = new System.Drawing.Point(102, 47);
+            this.TituloTextBox.Name = "TituloTextBox";
+            this.TituloTextBox.Size = new System.Drawing.Size(200, 20);
+            this.TituloTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Titulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Produccion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Pais";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Genero";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Año";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Descripcion";
+            // 
+            // textProduccion
+            // 
+            this.textProduccion.Location = new System.Drawing.Point(102, 138);
+            this.textProduccion.Name = "textProduccion";
+            this.textProduccion.Size = new System.Drawing.Size(200, 20);
+            this.textProduccion.TabIndex = 8;
+            // 
+            // comboPais
+            // 
+            this.comboPais.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboPais.FormattingEnabled = true;
+            this.comboPais.Items.AddRange(new object[] {
+            "Afganistán",
+            "Albania",
+            "Alemania",
+            "Andorra",
+            "Angola",
+            "Antigua y Barbuda",
+            "Arabia Saudita",
+            "Argelia",
+            "Argentina",
+            "Armenia",
+            "Australia",
+            "Austria",
+            "Azerbaiyán",
+            "Bahamas",
+            "Bangladés",
+            "Barbados",
+            "Baréin",
+            "Bélgica",
+            "Belice",
+            "Benín",
+            "Bielorrusia",
+            "Birmania",
+            "Bolivia",
+            "Bosnia y Herzegovina",
+            "Botsuana",
+            "Brasil",
+            "Brunéi",
+            "Bulgaria",
+            "Burkina Faso",
+            "Burundi",
+            "Bután",
+            "Cabo Verde",
+            "Camboya",
+            "Camerún",
+            "Canadá",
+            "Catar",
+            "Chad",
+            "Chile",
+            "China",
+            "Chipre",
+            "Ciudad del Vaticano",
+            "Colombia",
+            "Comoras",
+            "Corea del Norte",
+            "Corea del Sur",
+            "Costa de Marfil",
+            "Costa Rica",
+            "Croacia",
+            "Cuba",
+            "Dinamarca",
+            "Dominica",
+            "Ecuador",
+            "Egipto",
+            "El Salvador",
+            "Emiratos Árabes Unidos",
+            "Eritrea",
+            "Eslovaquia",
+            "Eslovenia",
+            "España",
+            "Estados Unidos",
+            "Estonia",
+            "Etiopía",
+            "Filipinas",
+            "Finlandia",
+            "Fiyi",
+            "Francia",
+            "Gabón",
+            "Gambia",
+            "Georgia",
+            "Ghana",
+            "Granada",
+            "Grecia",
+            "Guatemala",
+            "Guyana",
+            "Guinea",
+            "Guinea ecuatorial",
+            "Guinea-Bisáu",
+            "Haití",
+            "Honduras",
+            "Hungría",
+            "India",
+            "Indonesia",
+            "Irak",
+            "Irán",
+            "Irlanda",
+            "Islandia",
+            "Islas Marshall",
+            "Islas Salomón",
+            "Israel",
+            "Italia",
+            "Jamaica",
+            "Japón",
+            "Jordania",
+            "Kazajistán",
+            "Kenia",
+            "Kirguistán",
+            "Kiribati",
+            "Kuwait",
+            "Laos",
+            "Lesoto",
+            "Letonia",
+            "Líbano",
+            "Liberia",
+            "Libia",
+            "Liechtenstein",
+            "Lituania",
+            "Luxemburgo",
+            "Madagascar",
+            "Malasia",
+            "Malaui",
+            "Maldivas",
+            "Malí",
+            "Malta",
+            "Marruecos",
+            "Mauricio",
+            "Mauritania",
+            "México",
+            "Micronesia",
+            "Moldavia",
+            "Mónaco",
+            "Mongolia",
+            "Montenegro",
+            "Mozambique",
+            "Namibia",
+            "Nauru",
+            "Nepal",
+            "Nicaragua",
+            "Níger",
+            "Nigeria",
+            "Noruega",
+            "Nueva Zelanda",
+            "Omán",
+            "Países Bajos",
+            "Pakistán",
+            "Palaos",
+            "Panamá",
+            "Papúa Nueva Guinea",
+            "Paraguay",
+            "Perú",
+            "Polonia",
+            "Portugal",
+            "Reino Unido",
+            "República Centroafricana",
+            "República Checa",
+            "República de Macedonia",
+            "República del Congo",
+            "República Democrática del Congo",
+            "República Dominicana",
+            "República Sudafricana",
+            "Ruanda",
+            "Rumanía",
+            "Rusia",
+            "Samoa",
+            "San Cristóbal y Nieves",
+            "San Marino",
+            "San Vicente y las Granadinas",
+            "Santa Lucía",
+            "Santo Tomé y Príncipe",
+            "Senegal",
+            "Serbia",
+            "Seychelles",
+            "Sierra Leona",
+            "Singapur",
+            "Siria",
+            "Somalia",
+            "Sri Lanka",
+            "Suazilandia",
+            "Sudán",
+            "Sudán del Sur",
+            "Suecia",
+            "Suiza",
+            "Surinam",
+            "Tailandia",
+            "Tanzania",
+            "Tayikistán",
+            "Timor Oriental",
+            "Togo",
+            "Tonga",
+            "Trinidad y Tobago",
+            "Túnez",
+            "Turkmenistán",
+            "Turquía",
+            "Tuvalu",
+            "Ucrania",
+            "Uganda",
+            "Uruguay",
+            "Uzbekistán",
+            "Vanuatu",
+            "Venezuela",
+            "Vietnam",
+            "Yemen",
+            "Yibuti",
+            "Zambia",
+            "Zimbabue"});
+            this.comboPais.Location = new System.Drawing.Point(102, 179);
+            this.comboPais.Name = "comboPais";
+            this.comboPais.Size = new System.Drawing.Size(200, 21);
+            this.comboPais.TabIndex = 9;
+            // 
+            // comboGenero
+            // 
+            this.comboGenero.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboGenero.FormattingEnabled = true;
+            this.comboGenero.Items.AddRange(new object[] {
+            "animación ",
+            "artes marciales ",
+            "aventura ",
+            "bélico ",
+            "biográficas ",
+            "ciencia ficción ",
+            "comedia ",
+            "dibujos animados ",
+            "documentales ",
+            "drama ",
+            "erótico ",
+            "fantasía ",
+            "histórico ",
+            "infantil ",
+            "intriga ",
+            "musical ",
+            "oeste ",
+            "policíaca ",
+            "porno ",
+            "religioso ",
+            "romántico ",
+            "terror ",
+            "clásicas ",
+            "guerra ",
+            "drama general ",
+            "drama suspenso ",
+            "animación computarizada ",
+            "drama romance ",
+            "horror ",
+            "drama comedia ",
+            "drama político ",
+            "suspenso ",
+            "Gangster’s ",
+            "Western ",
+            "deportes ",
+            "misterio ",
+            "drama judicial ",
+            "épica ",
+            "Thriller"});
+            this.comboGenero.Location = new System.Drawing.Point(102, 226);
+            this.comboGenero.Name = "comboGenero";
+            this.comboGenero.Size = new System.Drawing.Size(130, 21);
+            this.comboGenero.TabIndex = 11;
+            // 
+            // richTextDescripcion
+            // 
+            this.richTextDescripcion.Location = new System.Drawing.Point(102, 313);
+            this.richTextDescripcion.Name = "richTextDescripcion";
+            this.richTextDescripcion.Size = new System.Drawing.Size(200, 86);
+            this.richTextDescripcion.TabIndex = 12;
+            this.richTextDescripcion.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Director";
+            // 
+            // textDirector
+            // 
+            this.textDirector.Location = new System.Drawing.Point(102, 91);
+            this.textDirector.Name = "textDirector";
+            this.textDirector.Size = new System.Drawing.Size(200, 20);
+            this.textDirector.TabIndex = 14;
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(289, 446);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(62, 23);
+            this.buttonLimpiar.TabIndex = 15;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(102, 269);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // formRegistro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 481);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.textDirector);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextDescripcion);
+            this.Controls.Add(this.comboGenero);
+            this.Controls.Add(this.comboPais);
+            this.Controls.Add(this.textProduccion);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TituloTextBox);
+            this.Controls.Add(this.buttonRegistrar);
+            this.Name = "formRegistro";
+            this.Text = "Registro de Pelicula";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRegistrar;
+        private System.Windows.Forms.TextBox TituloTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textProduccion;
+        private System.Windows.Forms.ComboBox comboPais;
+        private System.Windows.Forms.ComboBox comboGenero;
+        private System.Windows.Forms.RichTextBox richTextDescripcion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textDirector;
+        private System.Windows.Forms.Button buttonLimpiar;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ registro/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace registro
+{
+    public partial class formRegistro : Form
+    {
+        public formRegistro()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonR_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desea Gurdar esta Pelicula","Confirmacion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            this.Close();
+        }
+
+        private void buttonLimpiar_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    ctrl.Text = "";
+                }
+
+                if (ctrl is ComboBox)
+                {
+                    ctrl.Text = "";
+                }
+
+                if (ctrl is RichTextBox)
+                {
+                    ctrl.Text = "";
+                }
+            }
+        }
+    }
+}
